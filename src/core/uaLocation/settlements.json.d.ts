12d0bb49e167b18f3c515cab22557aaea4f0d01e
Alexandre Annic <alexandre.annic1@gmail.com>
2023-08-23 11:07:07 +0200

    fix: Location settlements performance

---
+++ src/core/uaLocation/settlements.json.d.ts
@@ -0,0 +1,10 @@
+declare module 'settlements.json' {
+  const value: Record<string, {
+    iso: string
+    parent: string
+    ua: string
+    en: string
+    _5w: string
+  }>
+  export default value
+}
