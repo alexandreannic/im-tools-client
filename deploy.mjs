93ec0ffdf36b8f921a0b8c30dcfd8b831c29152f
Alexandre Annic <alexandre.annic1@gmail.com>
2023-09-14 10:51:42 +0300

    Update deploy.mjs to handle env variables

--- deploy.mjs
+++ deploy.mjs
@@ -1,11 +1,12 @@
-import { exec, execSync } from 'child_process'
+import {exec, execSync} from 'child_process'
 import * as fs from 'fs'
+import dotenv from 'dotenv'
+
+dotenv.config({path: '.env.local'})
 
 const config = {
   mainBranch: 'master',
-  gitServer: 'https://drc-imaa-ukr-tools.scm.azurewebsites.net:443/drc-imaa-ukr-tools.git',
-  username: '$drc-imaa-ukr-tools',
-  password: 'rJxEsj09zN3nqknoS9GpxBM1W4vykcqXoZrCNzGungbgKspNMqSePuno4EAY',
+  gitServer: `https://${encodeURIComponent(process.env.DEPLOY_USER)}:${encodeURIComponent(process.env.DEPLOY_PWD)}@drc-imaa-ukr-tools.scm.azurewebsites.net:443/drc-imaa-ukr-tools.git`,
 }
 
 const run = (cl) => {
@@ -32,25 +33,25 @@ const newversion = process.argv[2] ?? 'patch' //as 'patch' | 'minor' | 'major'
 const getPackageVersion = () => JSON.parse(fs.readFileSync('package.json', 'utf8')).version
 
 const isOnMainBranch = () => new RegExp(`${config.mainBranch}\s*\n*`).test(execSync('git branch --show-current').toString())
-  ; (async () => {
-    try {
+;(async () => {
+  try {
 
-      // if (!isOnMainBranch()) {
-      //   console.error(`You must be on branch ${config.mainBranch} to publish.`)
-      // } else if (!gitWorkspaceIsEmpty()) {
-      //   console.error(`Your git status must be clean before to publish.`)
-      // } else {
-      await run(`git remote remove azure`).catch(console.error)
-      await run(`git remote add azure ${config.gitServer}`)
-      await run(`git push -f azure ${config.mainBranch}`)
-      await run(`git remote remove azure`)
-      // await run(`git commit -m "Release ${getPackageVersion()}"`)
-      // await run(`git push https://${config.username}:${config.password}@${config.gitServer} master`)
-      console.info(`Successfully deployed!`)
-    } catch (e) {
-      console.error(e)
-      await run(`git remote remove azure`)
-    }
-  })()
+    // if (!isOnMainBranch()) {
+    //   console.error(`You must be on branch ${config.mainBranch} to publish.`)
+    // } else if (!gitWorkspaceIsEmpty()) {
+    //   console.error(`Your git status must be clean before to publish.`)
+    // } else {
+    await run(`git remote remove azure`).catch(console.error)
+    await run(`git remote add azure ${config.gitServer}`)
+    await run(`git push -f azure ${config.mainBranch}`)
+    await run(`git remote remove azure`)
+    // await run(`git commit -m "Release ${getPackageVersion()}"`)
+    // await run(`git push https://${config.username}:${config.password}@${config.gitServer} master`)
+    console.info(`Successfully deployed!`)
+  } catch (e) {
+    console.error(e)
+    await run(`git remote remove azure`)
+  }
+})()
 
 // toggle deploy 5
