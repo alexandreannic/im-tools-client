230d9d585995ce9a2a11ff41625593bafb726c1b
Alexandre Annic <alexandre.annic1@gmail.com>
2023-09-22 11:13:06 +0300

    clean

--- src/features/Database/KoboTable/useKoboSchema.ts
+++ src/features/Database/KoboTable/useKoboSchema.ts
@@ -1,11 +1,9 @@
 import {useMemo} from 'react'
-import {Arr, Enum, map, mapFor} from '@alexandreannic/ts-utils'
+import {Arr, mapFor} from '@alexandreannic/ts-utils'
 import {KoboApiForm, KoboQuestionSchema} from '@/core/sdk/server/kobo/KoboApi'
-import {ignoredColType} from '@/features/Database/Database'
 import {Utils} from '@/utils/utils'
 import {useI18n} from '@/core/i18n'
 import {Messages} from '@/core/i18n/localization/en'
-import {group} from 'd3-array'
 
 export type KoboSchemaHelper = ReturnType<typeof buildKoboSchemaHelper>
 
@@ -32,16 +30,11 @@ const isolateGroups = (survey: KoboApiForm['content']['survey']) => {
 
 export const buildKoboSchemaHelper = ({
   schema,
-  // repeatAsColumns,
-  // repeatAsRows,
   m
 }: {
-  // repeatAsColumns?: string[]
-  // repeatAsRows?: string
   schema: KoboApiForm,
   m: Messages
 }) => {
-  // const repeatAsColumnsIndex = new Set(repeatAsColumns)
   const idSchema = {
     name: 'id',
     label: mapFor(schema.content.translations.length, () => 'ID'),
@@ -63,27 +56,6 @@ export const buildKoboSchemaHelper = ({
   }
 
   const {groupSchemas, surveyCleaned} = isolateGroups(schema.content.survey)
-
-  // const groupSchemas = schema.content.survey.reduce<Record<string, KoboQuestionSchema[]>>((acc, _, i, arr) => {
-  //   if (_.type === 'begin_repeat') {
-  //     const groupQuestion: KoboQuestionSchema[] = [
-  //       idSchema,
-  //       submissionTimeSchema,
-  //     ]
-  //     // map(schema.content.survey.find(_ => _.name === 'start'), startSchema => {
-  //     //   groupQuestion.push(startSchema)
-  //     // })
-  //     // map(schema.content.survey.find(_ => _.name === 'end'), endSchema => {
-  //     //   groupQuestion.push(endSchema)
-  //     // })
-  //     for (let j = i + 1; arr[j].$xpath?.includes(_.name + '/') && j <= arr.length; j++) {
-  //       groupQuestion.push(arr[j])
-  //     }
-  //     acc[_.name] = groupQuestion
-  //   }
-  //   return acc
-  // }, {} as any)
-  // console.log('schema', isolateGroups(schema.content.survey))
 
   const sanitizedForm: KoboApiForm = {
     ...schema,
@@ -113,7 +85,6 @@ export const buildKoboSchemaHelper = ({
 
   const choicesIndex = Arr(schema.content.choices).groupBy(_ => _.list_name)
   const questionIndex = Arr(schema.content.survey).reduceObject<Record<string, KoboQuestionSchema>>(_ => [_.name, _])
-
 
   return {
     groupsCount: Object.keys(groupSchemas).length,
