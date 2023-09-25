3d136e57207bdc6658f71b4dab9bf21c910f9216
Alexandre Annic <alexandre.annic1@gmail.com>
2023-09-25 11:44:47 +0300

    ok

--- src/shared/UkraineMap/oblastIndex.ts
+++ src/shared/UkraineMap/oblastIndex.ts
@@ -44,7 +44,7 @@ export class OblastIndex {
     'UA35': `Kirovohradska`,
     'UA80': `Kyiv`,
     'UA32': `Kyivska`,
-    'UA44': `Luhanska`, // UA-09 in Real but UA-44 in Activity Info
+    'UA09': `Luhanska`, // UA-09 in Real but UA-44 in Activity Info
     'UA46': `Lvivska`,
     'UA48': `Mykolaivska`,
     'UA51': `Odeska`,
